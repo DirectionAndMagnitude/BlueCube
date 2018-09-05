21128beca4b72a9d84b3724eed780983aa2d977b
elAmericano <jerome.crocco@gmail.com>
2018-09-05 10:19:53 -0600

    updating _new_exp activity
    Updating new_exp_activity to include persistant data

--- app/src/main/java/com/example/jeromecrocco/bluecube/_study_new_exp.java
+++ app/src/main/java/com/example/jeromecrocco/bluecube/_study_new_exp.java
@@ -7,9 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -23,14 +25,16 @@ public class _study_new_exp extends AppCompatActivity {
 
 
     private LinearLayout parentLinearLayout;
-    Type type = new TypeToken<ArrayList<String>>() {}.getType();
 
-    String text;
-    String spinner;
-    Gson gson = new Gson();
+
+    Type type = new TypeToken<ArrayList<String>>() {}.getType();  // Type of Exp Value
+    String text; // Entered Text Description
+    String spinner; // Spinner Object String
+    Gson gson = new Gson(); // For data conversion to / from SQLite database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         setContentView(R.layout.activity_study_new_exp);
         parentLinearLayout = (LinearLayout) findViewById(R.id.study_new_exp);
@@ -39,20 +43,62 @@ public class _study_new_exp extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-/*  For loading experimental data from JSON file
+        /*  For loading experimental data from JSON file
         Get the String from the SQLiteDatabse what you saved and changed into ArrayList type like below:
         outputarray is a String which is get from SQLiteDatabase for this example.*/
 
-        //
         _study_class study = _study_class.getInstance();
 
-        if (study.getExpText() != null)
+        text = study.getExpText();
 
-            text = study.getExpText();
+/*        if (text == null) {
+            //Initialize
+            setContentView(R.layout.activity_study_new_exp);
+            parentLinearLayout = (LinearLayout) findViewById(R.id.study_new_exp);
+        }*/
+        if (text != null) {
+
+            //Retrieve the data from the class
+
             spinner = study.getExpType();
-            ArrayList<String> finalTextString    =  gson.fromJson(text, type);
-            ArrayList<String> finalSpinnerString =  gson.fromJson(spinner, type);
+            ArrayList<String> finalTextString = gson.fromJson(text, type);
+            ArrayList<String> finalSpinnerString = gson.fromJson(spinner, type);
 
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    _study_new_exp.this,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    finalSpinnerString);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            //Re-populate the spinner / text boxes with the previously input data
+            for (int i = 0; i < finalTextString.size(); i++) {
+
+
+                // Get string-pair from each list
+                String l = finalTextString.get(i);
+                String s = finalSpinnerString.get(i);
+
+                // Insert a new row into the layout
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View rowView = inflater.inflate(R.layout.content_field, null);
+                parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - 1);
+
+                // Get the objects for each row
+                EditText edit = (EditText) rowView.findViewById(R.id.edit_text);
+                Spinner type = (Spinner) rowView.findViewById(R.id.type_spinner);
+
+
+                // Set the objects for each row
+                edit.setText(l);
+                type.setAdapter(adapter);
+                if (s != null) {
+                    int spinnerPosition = adapter.getPosition(s);
+                    type.setSelection(spinnerPosition);
+                }
+
+            }
+        }
             
 
 
@@ -85,27 +131,42 @@ public class _study_new_exp extends AppCompatActivity {
 
         for(int i=0; i<count; i++) {
             View vnew = parentLinearLayout.getChildAt(i);
+/*
             if (vnew instanceof EditText)
                 edit_text.add(v.toString());
             if (vnew instanceof Spinner)
                 spinner_text.add(v.toString());
+*/
 
-        //Convert to JSON
-        Gson gson = new Gson();
-        String text= gson.toJson(edit_text);
-        String spinner= gson.toJson(spinner_text);
-        study.setExpData(text,spinner);
+            // Get the objects for each row
+            EditText e = (EditText) vnew.findViewById(R.id.edit_text);
+            if (e != null) {
+                Spinner t = (Spinner) vnew.findViewById(R.id.type_spinner);
+                edit_text.add(e.getText().toString());
 
-        //Update SQLITE Table
-        dbHandler.addHandler(study,"exp");
+                TextView textView = (TextView) t.getSelectedView();
+                String result = textView.getText().toString();
+                spinner_text.add(result);
 
-        //Navigate back to study menu
-        setContentView(R.layout.activity_study_new);
-        Intent intent = new Intent(this, _study_new.class);
-        startActivity(intent);
+            }
+        }
 
+        if (edit_text != null) {
+            //Convert to JSON
+            Gson gson = new Gson();
+            String text = gson.toJson(edit_text);
+            String spinner = gson.toJson(spinner_text);
+            study.setExpData(text, spinner);
+
+            //Update SQLITE Table
+            dbHandler.addHandler(study, "exp");
+
+            //Navigate back to study menu
+            setContentView(R.layout.activity_study_new);
+            Intent intent = new Intent(this, _study_new.class);
+            startActivity(intent);
+        }
     }
 
 
-}
 }
